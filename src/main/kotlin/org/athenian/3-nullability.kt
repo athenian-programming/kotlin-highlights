a9d016854ebcc5ee49f51d77cf7c6cbfaec304f1
Paul Ambrose <pambrose@mac.com>
2019-07-16 23:30:36 +0700

    Misc updates

--- src/main/kotlin/org/athenian/3-nullability.kt
+++ src/main/kotlin/org/athenian/3-nullability.kt
@@ -4,8 +4,8 @@ fun main() {
 
     // Say good-bye to the dreaded NPE
     // Nullability is built into the type system
-    var a: String = "Non-null String"
-    var b: String? = "Nullable string"
+    var a: String = "Non-null string var"
+    var b: String? = "Nullable string var"
 
     //a = null
     //a = b
@@ -14,14 +14,9 @@ fun main() {
     if (b != null)
         a = b
 
-    // Elvis operator for null values
-    a = b ?: "Found a null value";
-    println(a)
+    // Null propagation
+    a = b?.toUpperCase() ?: "Found a null value";
 
     // Trust me, I know what I am doing -- all bets are off for NPE
     a = b!!
-
-    // Null propagation
-    val len = b.slice(2..6).slice(0..3).length ?: 0
-    println(len)
 }
