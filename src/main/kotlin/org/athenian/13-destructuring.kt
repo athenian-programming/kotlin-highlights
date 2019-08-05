716fc007b5936fe19c6903855dff6d3f7a2dab15
Paul Ambrose <pambrose@mac.com>
2019-08-05 15:58:41 -0700

    Add destructuring example

--- src/main/kotlin/org/athenian/13-destructuring.kt
+++ src/main/kotlin/org/athenian/13-destructuring.kt
@@ -9,6 +9,7 @@ data class Person(val name: String, val age: Int)
 class Location(val latitude: Double, val longitude: Double) {
     operator fun component1() = latitude
     operator fun component2() = longitude
+    operator fun component3() = "A string using $latitude and $longitude"
 }
 
 class RandomVals() {
@@ -21,9 +22,10 @@ class RandomVals() {
 
 operator fun RandomVals.component1() = randomVal1
 operator fun RandomVals.component2() = randomVal2
+operator fun RandomVals.component3() = randomVal1 + randomVal2
+
 
 fun main() {
-
     // Kotlin data classes include destructuring calls automatically
     val person = Person("Thomas", 40)
     val (name, age) = person
@@ -31,12 +33,15 @@ fun main() {
 
     // Regular classes can add destructuring support by overloading the componentN operators
     val loc = Location(38.2, 110.8)
-    val (lat, long) = loc
-    println("Lat/Long = $lat/$long")
+    val (lat, long, desc) = loc
+    println("Lat/Long = $lat/$long $desc")
 
     // You can add destructuring support to existing classes with extension functions
-    val randomVals = RandomVals()
-    val (val1, val2) = randomVals
-    println("Random vals = $val1 and $val2")
+    val randomVal = RandomVals()
+    val (val1, val2, sum) = randomVal
+    println("val1, val2, sum = $val1 and $val2 and $sum")
 
+    // You can use destructured assignments in loops as well
+    for ((v1, v2, s) in List(5) { RandomVals() })
+        println("v1, v2, s = $v1 and $v2 and $s")
 }
