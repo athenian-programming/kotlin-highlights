254abfc3df46b2026ad63c1eb80d59d3f20256fd
Paul Ambrose <pambrose@mac.com>
2019-08-06 20:38:07 -0700

    Update extensions

--- src/main/kotlin/org/athenian/11-extensions.kt
+++ src/main/kotlin/org/athenian/11-extensions.kt
@@ -28,6 +28,12 @@ fun String.halfCase(lowerFirst: Boolean = true): String {
         firstHalf.toUpperCase() + secondHalf.toLowerCase()
 }
 
+// Extension function for any type of object
+fun <T> T.foo() = "foo $this"
+
+// Extension function for constrained generic type
+fun <T : Number> T.doubleIt(): Double = this.toDouble() * 2
+
 fun main() {
     println((0..5).map { "$it is ${if (it.isEven) "Even" else "Odd"}" })
 
@@ -37,4 +43,7 @@ fun main() {
 
     println("halfCase() = ${"ThisISaTEST".halfCase()}")
     println("halfCase(false) = ${"ThisISaTEST".halfCase(false)}")
+
+    println("Extension function for any object receiver: ${"Test".foo()}")
+    println("Extension function for a number receiver: ${4.doubleIt()} and ${32.1.doubleIt()}")
 }
