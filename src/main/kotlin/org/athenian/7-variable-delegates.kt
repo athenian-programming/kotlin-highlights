08ccd61979901751862ae545d896333ebd64d26e
Paul Ambrose <pambrose@mac.com>
2019-09-01 12:02:58 -0700

    Add accessor count to variable delegate

--- src/main/kotlin/org/athenian/7-variable-delegates.kt
+++ src/main/kotlin/org/athenian/7-variable-delegates.kt
@@ -1,13 +1,19 @@
 package org.athenian
 
 import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.reflect.KProperty
 
 
 class User(objMap: Map<String, Any?>) {
 
     class TimestampedString(var content: String = "") {
-        operator fun getValue(thisRef: Any?, property: KProperty<*>) = content
+        private val counter = AtomicInteger(0)
+
+        operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+            val cnt = counter.incrementAndGet()
+            return "$content [Accessed $cnt time${if (cnt == 1) "" else "s"}]"
+        }
 
         operator fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
             content = "${LocalDateTime.now()}: $value"
@@ -44,5 +50,6 @@ fun main() {
     u.desc = "A new value"
     println("Desc = ${u.desc}")
     u.desc = "A second new value"
+    println("Desc = ${u.desc}")
     println("Desc = ${u.desc}")
 }
