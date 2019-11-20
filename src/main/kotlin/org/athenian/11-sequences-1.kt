b19edf84e985b356cd2d9b9a014a77070488ca2e
Paul Ambrose <pambrose@mac.com>
2019-11-20 14:26:27 -0800

    Misc cleanup

---
+++ src/main/kotlin/org/athenian/11-sequences-1.kt
@@ -0,0 +1,25 @@
+package org.athenian
+
+fun main() {
+
+    fun doReMi(): Sequence<String> =
+        sequence {
+            listOf("Do", "Re", "Mi", "Fa", "Sol", "La", "Ti", "Do")
+                .forEach {
+                    yield(it)
+                }
+        }
+
+    for (note in doReMi())
+        println(note)
+
+
+    val everyOther =
+        doReMi()
+            .mapIndexed { i, note -> if (i % 2 == 0) note else "" }
+            .filter { it.isNotEmpty() }
+            .toList()
+
+    println("Every other note: $everyOther")
+
+}
